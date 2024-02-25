05fdb30aa4c10a78ba4849bf68b1e27c6c2a15db
nxhettry <nishan.gtsn@gmail.com>
2024-02-25 17:27:51 +0545

    Foe future reference

--- main.js
+++ main.js
@@ -9,13 +9,15 @@ app.use("/blog", blog);
 
 //Creating first custom middleware
 app.use((req, res, next) => {
+    //this adds a log file with the timestamp and the method of request
     fs.appendFileSync("log_main.txt", `Timestamp : ${Date.now()}\n request type : ${req.method}\n\n`);
     next();
 });
 
 // creating second custom middleware
 app.use((req, res, next) => {
-    req.Name = " I am a programmer."; //This line will modify the request and add the string in it so that when it is accessed below, the modified string will kick in.
+    //This line will modify the request and add the string in it so that when it is accessed below, the modified string will kick in.
+    req.Name = " I am a programmer.";
     next();
 });
 
